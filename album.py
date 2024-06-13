dbacd6212a339c36a979beb25f63aedd3aab7038
RafaZanella <rzanella75@gmail.com>
2024-06-13 14:31:41 -0300

    first commit

---
+++ album.py
@@ -0,0 +1,10 @@
+class Album:
+    def __init__(self, titulo, lançamento, faixas):
+       self.titulo = titulo
+       self.lançamento = lançamento
+       self.faixas = faixas
+    def mostrar_info(selff):
+        print(f'Album: {self.titulo}')
+        self.lançamento.mostrar_info()
+
+
